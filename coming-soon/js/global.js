0efa96860346a9c2f7335708caddb038c812e31c
aungwanakyimal <59996144+aungwanakyimal@users.noreply.github.com>
2020-07-23 21:14:46 +0630

    Update global.js

--- coming-soon/js/global.js
+++ coming-soon/js/global.js
@@ -13,7 +13,7 @@ $( function() {
 		]
 	} )( 'overlay' );
 	
-        var endDate = "August 11, 2020 15:03:25";
+        var endDate = "August 18, 2020 15:03:25";
 
         $('.countdown.simple').countdown({ date: endDate });
 
